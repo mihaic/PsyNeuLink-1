mihaic/PsyNeuLink-1

commit 1879fdc4733219eb47778e6bb627685ce39f7cea

add basic code for adding volatile param values to composition params_by_execution_id

PsyNeuLink/Components/Component.py
@@ -750,6 +750,7 @@ class Component(object):
         self.recording = False
         # Used by run to store return value of execute
         self.results = []
+        self.compositions = set()
 
 
         # ENFORCE REQUIRED CLASS DEFAULTS
@@ -1851,6 +1852,26 @@ class Component(object):
         elif mode == ResetMode.ALL_TO_CLASS_DEFAULTS:
             self.params_current = self.paramClassDefaults.copy()
             self.paramInstanceDefaults = self.paramClassDefaults.copy()
+
+    def update_volatile_params(self, composition=None, execution_id=None):
+        if composition is None:
+            comps = self.compositions
+        else:
+            comps = [composition]
+
+        if execution_id is None:
+            ids = [c._execution_id for c in self.compositions]
+        else:
+            ids = [execution_id]
+
+        for c in comps:
+            for eid in ids:
+                if self not in c.params_by_execution_id[eid]:
+                    c.params_by_execution_id[eid][self] = {}
+
+                for param in self.params_volatile.values:
+                    c.params_by_execution_id[eid][self][param] = getattr(self, param)
+
 
     def _validate_variable(self, variable, context=None):
         """Validate variable and assign validated values to self.variable

PsyNeuLink/Composition.py
@@ -323,7 +323,9 @@ class Composition(object):
         self._scheduler_processing = None
         self._scheduler_learning = None
 
+        self._execution_id = self._get_unique_id()
         self.params_by_execution_id = {}
+        self.params_by_execution_id[self._execution_id] = {}
 
         # status attributes
         self.graph_consistent = True  # Tracks if the Composition is in a state that can be run (i.e. no dangling projections, (what else?))
@@ -418,6 +420,9 @@ class Composition(object):
             self.mechanisms.append(mech)
             self.mechanisms_to_roles[mech] = set()
 
+            mech.compositions.add(self)
+            mech.update_volatile_params(composition=self)
+
             self.needs_update_graph = True
             self.needs_update_graph_processing = True
             self.needs_update_scheduler_processing = True
@@ -448,6 +453,9 @@ class Composition(object):
             self.graph.connect_components(sender, projection)
             self.graph.connect_components(projection, receiver)
             self._validate_projection(sender, projection, receiver)
+
+            projection.compositions.add(self)
+            projection.update_volatile_params(composition=self)
 
             self.needs_update_graph = True
             self.needs_update_graph_processing = True
@@ -745,7 +753,7 @@ class Composition(object):
 
         # Traverse processing graph and assign one uuid to all of its mechanisms
         if execution_id is None:
-            execution_id = self._get_unique_id()
+            execution_id = self._execution_id
 
         if execution_id not in self.execution_ids:
             self.execution_ids.append(execution_id)
@@ -762,6 +770,9 @@ class Composition(object):
         # Assign the uuid to all input mechanisms
         for k in self.input_mechanisms.keys():
             self.input_mechanisms[k]._execution_id = execution_id
+
+            if self.input_mechanisms[k] not in self.params_by_execution_id[execution_id]:
+                self.params_by_execution_id[execution_id][self.input_mechanisms[k]] = {}
 
         self._execution_id = execution_id
         return execution_id

commit 0fd087e33b209a1fa77cd73b456a5a16849dfdb6

fix bug in update_volatile_params introduced in 1879fdc4

PsyNeuLink/Components/Component.py
@@ -1897,7 +1897,7 @@ class Component(object):
             comps = [composition]
 
         if execution_id is None:
-            ids = [c._execution_id for c in self.compositions]
+            ids = [c._execution_id for c in comps]
         else:
             ids = [execution_id]
 